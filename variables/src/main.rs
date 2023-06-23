ea3909552e43c94f445a1427703b74295ebe0ccc
vineet-agarwa11 <vineetagarwal2402@gmail.com>
2023-06-22 23:37:51 -0700

    Chapter 3 - 3.1 Variables and Mutability (complete)

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,22 @@
 fn main() {
+    // Mutability
+    println!("--Mutability--");
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+
+    // Shadowing
+    println!("--Shadowing--");
+    let y = 1;
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+
+    println!("The value of y is: {y}");
+
 }
